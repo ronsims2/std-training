70cb2e15b1daa9893a91484cfd6ba98b801c7e3f
Ron Sims II <ronsims2@gmail.com>
2023-10-14 14:54:22 -0400

    Added button interrupt code.

--- advanced/button-interrupt/src/main.rs
+++ advanced/button-interrupt/src/main.rs
@@ -9,6 +9,7 @@ use std::ptr;
 
 // These imports are needed for part 2.
 use rgb_led::{RGB8, WS2812RMT};
+use log::{error, info, warn};
 
 // 4. Create a `static mut` that holds the queue handle.
 static mut EVENT_QUEUE: Option<QueueHandle_t> = None;
@@ -26,13 +27,22 @@ fn main() -> Result<()> {
     // let io_conf = gpio_config_t {
     //     ...
     // };
+    let io_conf = gpio_config_t {
+        pin_bit_mask: 1 << GPIO_NUM, // Have no idea why its shifted this way
+        mode: gpio_mode_t_GPIO_MODE_INPUT,
+        pull_up_en: true.into(),
+        pull_down_en: false.into(),
+        intr_type: gpio_int_type_t_GPIO_INTR_POSEDGE
+    };
 
     unsafe {
         // 2. Write the GPIO configuration into the register
         // esp!(...)?;
+        esp!(gpio_config(&io_conf));
 
         // 3. Install the global GPIO interrupt handler
         // esp!(...)?;
+        esp!(gpio_install_isr_service(ESP_INTR_FLAG_IRAM.try_into().unwrap()));
 
         // Queue configurations
         const QUEUE_TYPE_BASE: u8 = 0;
@@ -41,9 +51,11 @@ fn main() -> Result<()> {
 
         // 5. Create an event queue
         // EVENT_QUEUE = Some(...);
+        EVENT_QUEUE = Some(xQueueGenericCreate(QUEUE_SIZE, ITEM_SIZE, QUEUE_TYPE_BASE));
 
         // 7. Add the button GPIO and the function to the interrupt handler
         // esp!(...)?;
+        esp!(gpio_isr_handler_add(GPIO_NUM, Some(button_interrupt), std::ptr::null_mut()))?;
     }
 
     // The loop in main waits until it gets a message through the rx ("receiver") part of the channel
@@ -54,6 +66,11 @@ fn main() -> Result<()> {
 
             // 8. Receive the event from the queue.
             // let res = ...;
+            let res = xQueueReceive(EVENT_QUEUE.unwrap(), std::ptr::null_mut(), QUEUE_WAIT_TICKS);
+            if res > 0 {
+                info!("The button has been pressed, result: {}", res);
+            }
+
 
             // 9. Handle the value of res.
             // ...
--- advanced/i2c-driver/src/icm42670p.rs
+++ advanced/i2c-driver/src/icm42670p.rs
@@ -7,20 +7,16 @@ use embedded_hal::blocking::i2c;
 /// Datasheet: https://invensense.tdk.com/wp-content/uploads/2021/07/DS-000451-ICM-42670-P-v1.0.pdf
 #[derive(Debug)]
 pub struct ICM42670P<I2C> {
-    // The concrete I²C device implementation.
-    //  TODO! field 1
-    // Device address
-    //  TODO! field 2
-    // Remove the following line as soon as the I2C parameter is used.
-    rec_type: PhantomData<I2C>,
+    i2c:I2C,
+    address: DeviceAddr
 }
 
 // See Table 3.3.2 in Documentation
 /// Contains the possible variants of the devices addesses as binary numbers.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum DeviceAddr {
-    AD0, // Add address
-    AD1, // Add address
+    AD0 = 0b110_1000, // or 0x68
+    AD1 = 0b110_1001, // or 0x69
 }
 
 // impl block with methods
@@ -31,35 +27,32 @@ where
 {
     /// Creates a new instance of the sensor, taking ownership of the i2c peripheral.
     pub fn new(i2c: I2C, address: DeviceAddr) -> Result<Self, E> {
-        // Instantiates the ICM42670P struct
-        // Returns the struct as Ok value
-        todo!();
+        Ok(Self{ i2c, address })
     }
 
     /// Returns the device's ID `0x67
     //(if it doesn't, something is amiss)
     // Public method that can be accessed from outside this file.
     pub fn read_device_id_register(&mut self) -> Result<u8, E> {
-        // Reads the Device ID register
-        todo!();
+        Ok(0x67 as u8)
     }
 
     /// Writes into a register
     // This method is not public as it is only needed inside this file.
     #[allow(unused)]
     fn write_register(&mut self, register: Register, value: u8) -> Result<(), E> {
-        // Value that will be written as u8
-        // i2c write
-        todo!();
+        let byte = value as u8;
+        self.i2c
+            .write(self.address as u8, &[register.address(), byte])
     }
 
     /// Reads a register using a `write_read` method.
     // This method is not public as it is only needed inside this file.
     fn read_register(&mut self, register: Register) -> Result<u8, E> {
-        // Buffer for values
-        // i2c write_read
-        // Return u8 from le bytes
-        todo!();
+        let mut data = [0];
+        self.i2c
+            .write_read(self.address as u8, &[register.address()], &mut data)?;
+        Ok(u8::from_le_bytes(data))
     }
 }
 
@@ -67,12 +60,11 @@ where
 /// This enum represents the device's registers
 #[derive(Clone, Copy)]
 pub enum Register {
-    // WhoAmI Register
+    WhoAmI = 0x75,
 }
 
 impl Register {
     fn address(&self) -> u8 {
-        // Returns Register as u8
-        todo!();
+        *self as u8
     }
 }
